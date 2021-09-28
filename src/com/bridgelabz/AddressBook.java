96046da7223020668dd6658dcd6a36e986f2acf0
KumarB95 <kumarbalingal@gmail.com>
2021-09-28 19:46:15 +0530

    Modified : Taken inputs from User required for Address book and displayed

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,7 +1,14 @@
 package com.bridgelabz;
+/*
+* Program : Address book program
+* Author : Navalkumar Balingal
+* Date : 28th Sep'21
+* */
 
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to Address book Program");
+        Utility utility=new Utility();
+        utility.contactDetails();
     }
 }
